21cf49ded8cb52d49c247853a9dc578ea3fd6ce3
Сергей Григорьев <92942323+Sergey17777@users.noreply.github.com>
2022-02-14 16:01:21 +0500

    Update task.js

--- task.js
+++ task.js
@@ -139,13 +139,5 @@ class Student {
         return sumOfAveMarks / countOfSubjects;
     }
 
-    /*версия от Сергея Кундрюкова:
-    getAverage() {
-        const subjects = Object.keys(this.#journal)
-        let sumOfAveMarks = 0;
-        subjects.forEach(([subject]) => {
-            sumOfAveMarks += this.getAverageBySubject(subject);
-        });
-        return sumOfAveMarks / subjects.length;
-    } */
+    
 }
